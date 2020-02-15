f683deee896612526aaf3f43f3cb99b051f218c2
dhiyo7 <dhiyoreksa12@gmail.com>
2020-02-15 22:25:44 +0700

    Hellow World

---
+++ Hello World/src/HelloWorld.kt
@@ -0,0 +1,10 @@
+/* Bikin Main Function nya dulu */
+fun main (){
+    println("Hello Temen Temen PLUGIN")
+    hello() //Panggil function hello() pada main function
+}
+
+/* Custom Function */
+fun hello (){
+    println("hello lagi")
+}
