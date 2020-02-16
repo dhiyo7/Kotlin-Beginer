7153b998a0625163508383feeba64eb2d1b166a4
dhiyo7 <dhiyoreksa12@gmail.com>
2020-02-16 17:58:27 +0700

    Tipe Data Angka

---
+++ Tipe_Data_Angka/src/TipeDataAngka.kt
@@ -0,0 +1,20 @@
+fun main () {
+
+    /* Tipe Data Angka Double */
+    val uang = 10.5
+    println("Ini Tipe data Double " + uang)
+
+    /* Tipe Data Angka Int */
+    val uang2 = 7
+    println("Ini Tipe data Int "+ uang2)
+
+    /* Contoh Operasi Aritmatika */
+    println(uang + uang2)
+    println(uang - uang2)
+    println(uang * uang2)
+    println(uang / uang2)
+    println(uang % uang2)
+    println(uang2 * 5 - 10)
+
+
+}
