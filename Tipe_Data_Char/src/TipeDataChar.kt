96935b2dab716d7fa6247f64af8047b635af4e66
dhiyo7 <dhiyoreksa12@gmail.com>
2020-02-16 21:44:11 +0700

    Tipe Data Char

---
+++ Tipe_Data_Char/src/TipeDataChar.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    /* Kalo pada String menggunakan (" ")
+    * pada Char menggunakan (' ') */
+
+    val data = '7'
+    val nama = "PLUGIN"
+    println(data)
+
+    /* Menggabungkan dengan String */
+    println("Ini Angka " + data)
+
+    /* Menghitung array index pada variable nama */
+    print(nama[4])
+}
